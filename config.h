8b9c511e3bf49684152f03a9cd86cd865cf92d43
jyand <jyand@protonmail.com>
2023-05-01 16:00:50 -0400

    initial stuff

---
+++ config.h
@@ -0,0 +1,17 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define PORT_NUM 1004
+#define MAX_ROOMS 5
+#define MAX_USERS 20
+
+struct ThreadArgs {
+        int clisockfd
+} ;
---
+++ main_server.c
@@ -0,0 +1,4 @@
+#include "config.h"
+
+int main(int argc, char **argv) {
+}
