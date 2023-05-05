09c75203cd4c6d2cbb34b4297bbc6c7f24bed5a0
Nate Sorvino <123539760+nsorvino@users.noreply.github.com>
2023-05-05 19:02:58 -0400

    Add files via upload

--- main_server.c
+++ main_server.c
@@ -1,169 +1,191 @@
 #include "config.h"
 
-char *colors[MAX_USERS] = {"\x1b[31m", "\x1b[32m", "\x1b[33m", "\x1b[34m", "\x1b[35m", "\x1b[36m", "\x1b[37m", "\x1b[41m", "\x1b[42m", "\x1b[43m", "\x1b[44m", "\x1b[45m", "\x1b[46m", "\x1b[47m"} ;
+char *colors[MAX_USERS] = {"\x1b[31m", "\x1b[32m", "\x1b[33m", "\x1b[34m", "\x1b[35m", "\x1b[36m", "\x1b[37m", "\x1b[41m", "\x1b[42m", "\x1b[43m", "\x1b[44m", "\x1b[45m", "\x1b[46m", "\x1b[47m"};
 
-struct UserNode *head = NULL ;
-struct UserNode *tail = NULL ;
+struct UserNode *user_head = NULL;
+struct UserNode *user_tail = NULL;
 
-/* store names of connected users organized by room  */
-char *rooms[MAX_ROOMS][MAX_USERS] ;
+struct ChatRoom {
+    char *room_code;
+    struct UserNode *user_head;
+    struct UserNode *user_tail;
+    struct ChatRoom *next;
+};
 
-int CreateNewRoom() {
-        bool newroom_avail = true ;
-        int k ;
-        for (k = 0 ; k < MAX_ROOMS ; ++k) {
-                for (int j = 0 ; j < MAX_USERS ; ++j) {
-                        if (rooms[k][j] != NULL) {
-                                newroom_avail = false ;
-                        }
-                }
-                if (new_room_avail == true) {
-                        return k ;
-                } else {
-                        error("Error! no new rooms available") ;
-                }
+struct ChatRoom *chatroom_head = NULL;
+struct ChatRoom *chatroom_tail = NULL;
+
+// Function to create a new chat room and add it to the list
+struct ChatRoom *create_chatroom(const char *room_code) {
+    struct ChatRoom *new_room = (struct ChatRoom *)malloc(sizeof(struct ChatRoom));
+    new_room->room_code = strdup(room_code);
+    new_room->user_head = NULL;
+    new_room->user_tail = NULL;
+    new_room->next = NULL;
+
+    if (chatroom_tail == NULL) {
+        chatroom_head = chatroom_tail = new_room;
+    } else {
+        chatroom_tail->next = new_room;
+        chatroom_tail = new_room;
+    }
+
+    return new_room;
+}
+
+// Function to find a chat room by its room code
+struct ChatRoom *find_chatroom_by_code(const char *room_code) {
+    struct ChatRoom *current = chatroom_head;
+    while (current != NULL) {
+        if (strcmp(current->room_code, room_code) == 0) {
+            return current;
         }
+        current = current->next;
+    }
+    return NULL;
+}
+
+// Function to add a user to a chat room
+void add_user_to_chatroom(struct ChatRoom *chatroom, struct UserNode *user) {
+    if (chatroom->user_tail == NULL) {
+        chatroom->user_head = chatroom->user_tail = user;
+    } else {
+        chatroom->user_tail->next = user;
+        chatroom->user_tail = user;
+    }
 }
 
 void ShowConnected() {
-        struct UserNode *current = head ;
-        if (head == NULL) {
-                printf("empty\n") ;
-        } else {
-                while (current != NULL) {
-                        printf("%s\n", current->username, current->room) ;
-                        current = current->next ;
-                }
+    struct UserNode *current = user_head;
+    if (user_head == NULL) {
+        printf("empty\n");
+    } else {
+        while (current != NULL) {
+            printf("%s\n", current->username);
+            current = current->next;
         }
+    }
 }
 
 // not unique for now
 char *AssignColor() {
-        srand((unsigned)"") ;
-        int k = rand()%MAX_USERS ;
-        return colors[k] ;
+    srand((unsigned)time(NULL));
+    int k = rand() % MAX_USERS;
+    return colors[k];
 }
 
-void AddTail(int newclisockfd, char *newusername, char *newcolor, int usr_roomnum) {
-        if (head == NULL) {
-                head = (struct UserNode*)malloc(sizeof(struct UserNode)) ;
-                head->clisockfd = newclisockfd ;
-                head->username = (char*)malloc(strlen(newusername)*sizeof(char)) ;
-                strcpy(head->username, newusername) ;
-                head->color = (char*)malloc(strlen(newcolor)*sizeof(char)) ;
-                strcpy(head->color, newcolor) ;
-                head->room = usr_roomnum ;
-                head->next = NULL ;
-                tail = head ;
-        } else {
-                tail->next = (struct UserNode*)malloc(sizeof(struct UserNode)) ;
-                tail->next->clisockfd = newclisockfd ;
-                tail->next->username = (char*)malloc(strlen(newusername)*sizeof(char)) ;
-                strcpy(tail->next->username, newusername) ;
-                tail->next->color = (char*)malloc(strlen(newcolor)*sizeof(char)) ;
-                strcpy(tail->next->color, newcolor) ;
-                tail->next->room = usr_roomnum ;
-                tail->next->next = NULL ;
-                tail = tail->next ;
-        }
+void AddTail(int newclisockfd, char *newusername, char *newcolor) {
+    if (user_head == NULL) {
+        user_head = (struct UserNode *)malloc(sizeof(struct UserNode));
+        user_head->clisockfd = newclisockfd;
+        user_head->username = (char *)malloc(strlen(newusername) * sizeof(char));
+        strcpy(user_head->username, newusername);
+        user_head->color = (char *)malloc(strlen(newcolor) * sizeof(char));
+        strcpy(user_head->color, newcolor);
+        user_head->next = NULL;
+        user_tail = user_head;
+    } else {
+        user_tail->next = (struct UserNode *)malloc(sizeof(struct UserNode));
+        user_tail->next->clisockfd = newclisockfd;
+        user_tail->next->username = (char *)malloc(strlen(newusername) * sizeof(char));
+        strcpy(user_tail->next->username, newusername);
+        user_tail->next->color = (char *)malloc(strlen(newcolor) * sizeof(char));
+        strcpy(user_tail->next->color, newcolor);
+        user_tail->next->next = NULL;
+        user_tail = user_tail->next;
+    }
 }
 
 void Broadcast(int fromfd, char *message) {
-        struct sockaddr_in cliaddr ;
-        socklen_t clen = sizeof(cliaddr) ;
-        if (getpeername(fromfd, (struct sockaddr*)&cliaddr, &clen) < 0) {
-                error("Error! Unkown sender") ;
+        struct sockaddr_in cliaddr;
+        socklen_t clen = sizeof(cliaddr);
+        if (getpeername(fromfd, (struct sockaddr *)&cliaddr, &clen) < 0) {
+                error("Error! Unknown sender");
         }
-        char *username ;
-        char *color ;
-        int room ;
-        struct UserNode *current = head ;
+        char *username;
+        char *color;
+        struct UserNode *current = user_head;
         while (current != NULL) {
                 if (current->clisockfd == fromfd) {
-                        username = current->username ;
-                        color = current->color ;
-                        room = current->room ;
-                        break ;
+                        username = current->username;
+                        color = current->color;
+                        break;
                 }
-                current = current->next ;
+                current = current->next;
         }
-        current = head ;
+        current = user_head;
         while (current != NULL) {
                 if (current->clisockfd != fromfd) {
-                        char buffer[512] ;
-                        sprintf(buffer, "\x1b[0m%s[%s@%s#%d]: %s\x1b[0m", color, username, inet_ntoa(cliaddr.sin_addr), room, message) ;
-                        int nmsg = strlen(buffer) ;
-                        int nsend = send(current->clisockfd, buffer, nmsg, 0) ;
+                        char buffer[512];
+                        sprintf(buffer, "\x1b[0m%s[%s@%s]: %s\x1b[0m", color, username, inet_ntoa(cliaddr.sin_addr), message);
+                        int nmsg = strlen(buffer);
+                        int nsend = send(current->clisockfd, buffer, nmsg, 0);
                         if (nsend != nmsg) {
-                                error("Error! send() failed") ;
+                                error("Error! send() failed");
                         }
                 }
-                current = current->next ;
+                current = current->next;
         }
 }
 
 void *ServerThread(void *args) {
-        pthread_detach(pthread_self()) ;
-        int clisockfd = ((struct ThreadArgs*)args)->clisockfd ;
-        free(args) ;
-        char buffer[256] ;
-        int nrcv = recv(clisockfd, buffer, 255, 0) ;
-        struct UserNode *current = head ;
-        struct UserNode *prev = head ;
+        pthread_detach(pthread_self());
+        int clisockfd = ((struct ThreadArgs *)args)->clisockfd;
+        struct ChatRoom *chatroom = ((struct ThreadArgs *)args)->chatroom;
+        free(args);
+        char buffer[256];
+        int nrcv = recv(clisockfd, buffer, 255, 0);
+        struct UserNode *current = user_head;
+        struct UserNode *prev = user_head;
         while (current != NULL) {
-                int temp = current->clisockfd ;
+                int temp = current->clisockfd;
                 if (temp == clisockfd) {
-                        break ;
+                        break;
                 }
-                if (current != head) {
-                        prev = prev->next ;
+                if (current != user_head) {
+                        prev = prev->next;
                 }
-                current = current->next ;
+                current = current->next;
         }
         if (nrcv < 0) {
-                error("Error! recv() failed") ;
+                error("Error! recv() failed");
         }
         if (nrcv == 0) {
-                printf("%s disconnected\n", current->username) ;
-                for (int k = 0 ; k < MAX_USERS ; ++k) {
-                        if (strcmp(current->username, rooms[current->room][k]) == 0) {
-                                rooms[current->room][k] = NULL ;
-                        }
-                }
-                if (current == head) {
-                        head = current->next ;
-                        free(current) ;
+                printf("%s disconnected\n", current->username);
+                if (current == user_head) {
+                        user_head = current->next;
+                        free(current);
                 } else {
-                        prev->next = current->next ;
-                        free(current) ;
+                        prev->next = current->next;
+                        free(current);
                 }
-                close(clisockfd) ;
-                return NULL ;
+                close(clisockfd);
+                return NULL;
         } else {
-                Broadcast(clisockfd, buffer) ;
+                Broadcast(clisockfd, buffer);
         }
         while (nrcv != 0) {
                 if (nrcv < 0) {
-                        error("Error! recv() failed") ;
+                        error("Error! recv() failed");
                 } else {
-                        Broadcast(clisockfd, buffer) ;
+                        Broadcast(clisockfd, buffer);
                 }
-                memset(buffer, 0, 256) ;
-                nrcv = recv(clisockfd, buffer, 255, 0) ;
+                memset(buffer, 0, sizeof(buffer));
+                nrcv = recv(clisockfd, buffer, 255, 0);
         }
-        printf("%s disconnected\n", current->username) ;
-        if (current == head) {
-                head = current->next ; 
-                free(current) ;
+        printf("%s disconnected\n", current->username);
+        if (current == user_head) {
+                user_head = current->next;
+                free(current);
         } else {
-                prev->next = current->next ;
-                free(current) ;
+                prev->next = current->next;
+                free(current);
         }
-        close(clisockfd) ;
-        return NULL ;
+        close(clisockfd);
+        return NULL;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char *argv[]) {
         int sockfd = socket(AF_INET, SOCK_STREAM, 0) ;
         if (sockfd < 0) {
                 error("Error! Coudn't open socket.") ;
@@ -179,39 +201,37 @@ int main(int argc, char **argv) {
                 error("Error! bind() failed") ;
         }
         listen(sockfd, 5) ;
-        for (;;) {
-                struct sockaddr_in cli_addr ;
-                socklen_t clen = sizeof(cli_addr) ;
-                int newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &clen) ;
-                if (newsockfd < 0) {
-                        error("Error! accept() failed") ;
+
+        while (1) {
+                struct sockaddr_in cliaddr;
+                socklen_t clen = sizeof(cliaddr);
+                int clisockfd = accept(sockfd, (struct sockaddr *)&cliaddr, &clen);
+                if (clisockfd < 0) {
+                    error("Error! accept() failed");
                 }
-                char roombuffer[256] ;
-                memset(roombuffer, 0, 256) ;
-                int nrcv = recv(newsockfd, roombuffer, 255, 0) ;
+
+                char username[256];
+                int nrcv = recv(clisockfd, username, 255, 0);
                 if (nrcv < 0) {
-                        error("Error! recv() failed") ;
+                    error("Error! recv() failed");
                 }
-                if (strcmp(roombuffer, "new") == 0) {
+                username[nrcv] = '\0';
+
+                printf("%s connected\n", username);
+
+                char *color = AssignColor();
+                AddTail(clisockfd, username, color);
+
+                struct ThreadArgs *args = (struct ThreadArgs *)malloc(sizeof(struct ThreadArgs));
+                args->clisockfd = clisockfd;
+                args->chatroom = NULL;  // No chat room for now
+
+                pthread_t thread_id;
+                if (pthread_create(&thread_id, NULL, ServerThread, (void *)args) != 0) {
+                    error("Error! pthread_create() failed");
                 }
-                char namebuffer[256] ;
-                memset(namebuffer, 0, 256) ;
-                int nrcv = recv(newsockfd, namebuffer, 255, 0) ;
-                if (nrcv < 0) {
-                        error("Error! recv() failed") ;
-                }
-                printf("Connected: %s@%s\n", namebuffer, inet_ntoa(cli_addr.sin_addr)) ;
-                AddTail(newsockfd, namebuffer, AssignColor()) ;
-                ShowConnected() ;
-                struct ThreadArgs *args = (struct ThreadArgs*)malloc(sizeof(struct ThreadArgs)) ;
-                if (args == NULL) {
-                        error("Error! Couldn't create thread argument.") ;
-                }
-                args->clisockfd = newsockfd ;
-                pthread_t tid ;
-                if (pthread_create(&tid, NULL, ServerThread, (void*)args) != 0) {
-                        error("Error! Couldn't create a new thread.") ;
-                }
-        }
-        return 0 ;
+            }
+
+        close(sockfd);
+        return 0;
 }
